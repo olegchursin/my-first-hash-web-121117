cc50f73891e4acd282a4c2f6d47f833079beb95a
Oleg Chursin <olegchursin@gmail.com>
2017-11-29 20:59:09 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,6 @@ def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
-  shipping_manifest["pearl necklace"] = 1
   shipping_manifest
   #your code here
   #remember to return the shipping_manifest hash
